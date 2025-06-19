3a005f1f03ec8742b78705362156cb1fb47ed633
Phan Hoàng Khang <togaphan@gmail.com>
2025-06-19 15:25:49 +0700

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,6 +1,7 @@
 const numDice = document.querySelector(".dice_number");
 const RollDice = document.querySelector(".dice_rolled");
 const imageDice = document.querySelector(".dice_image");
+const sumDice = document.querySelector(".sum_dice");  
 
 function rollDice() {
     let values = [];
@@ -11,8 +12,14 @@ function rollDice() {
         values.push(value);
         images.push(`<img src="Dices/Dice-${value}.png"></img>`);
     }
+    
+    let sum = 0;
+    for (value of values) {
+        sum+=value;
+    }
     if (numDice.value <=20) {
         RollDice.textContent = `Dice rolled: ${values.join(', ')}`;
+        sumDice.textContent= `Total Score: ${sum}`;
         imageDice.innerHTML = images.join('');
     }
     else {
@@ -24,7 +31,9 @@ function rollDice() {
 function reset() {
     let values = [];
     let images = [];
+    let sum = 0;
 
     RollDice.textContent = ``;
+    sumDice.textContent= ``;
     imageDice.innerHTML = ``;
 }
